4cecec11de3828e3aad0cf09881751f3258a4d60
HarrisL2 <harris.yh.lau@gmail.com>
2016-05-04 22:09:20 +0800

    Variables 1
    First test

--- Game.js
+++ Game.js
@@ -1,7 +1,12 @@
-var 
-
+var lifeforce = 0;
 
 function Initialize() {
+	while (true) {
+	document.getElementById("life"}.innerhtml = "Life force:" + lifeforce;
+	}
+}
 
+function a() {
+	lifeforce += 1;
 }
 
