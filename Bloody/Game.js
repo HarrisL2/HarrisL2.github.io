1b36530e00b7d8c2e076f9be509473ea7d00d0ce
HarrisL2 <harris.yh.lau@gmail.com>
2021-08-18 02:29:35 +0800

    Added Combat system

--- Bloody/Game.js
+++ Bloody/Game.js
@@ -20,14 +20,23 @@ Game.Launch = function()
 	Game.timeRaw = this.date.getTime();
 	Game.timeLast = this.date.getTime();
 	Game.BleedCost = 10;
-	Game.Blood = 0;
+	Game.Blood = 200;
+	Game.DamagePerSecond = 1;
+	Game.DamageUpCost = 10;
 	Game.BloodGainPerSecond = 1;
-
-
+	Game.isFighting = false;
+	Game.bossID = 0;
+	Game.bossNames = ["Lesser Demon", "Demon", "Greater Demon"]
+	Game.bossDamage = [1, 10, 100]
+	Game.bossHealth = [200, 5000, 250000]
+	Game.currBossHealth = 0;
 	/*==============================================================
 	  BUTTON ASSIGNMENT
 	==============================================================*/
 	AddEvent(l("sacrifice"), "click", Game.Bleed);
+	AddEvent(l("dmgup"), "click", Game.DamageUp);
+	AddEvent(l("fightDemon"), "click", Game.FightNextBoss);
+	AddEvent(l("leaveHell"), "click", Game.LeaveHell);
 	Game.Loop();
 
 }
@@ -38,7 +47,13 @@ Game.Draw = function()
 {
 	l("blood").innerHTML = "You have "+Game.Blood+" Blood.";
 	l("flow").innerHTML = "You create "+Game.BloodGainPerSecond+" Blood per second.";
-	l("cost").innerHTML = "It costs "+Game.BleedCost+" Blood to appease Him.";
+	l("costBleed").innerHTML = "It costs "+Game.BleedCost+" Blood to appease Him.";
+	l("damage").innerHTML = "You deal "+Game.DamagePerSecond+" damage to enemies per second.";
+	l("hellDamage").innerHTML = "You are dealing "+Game.DamagePerSecond+" damage to "+Game.bossNames[Game.bossID]+" per second.";
+	l("costDmgup").innerHTML = "It costs "+Game.DamageUpCost+" Blood to increase your damage.";
+	l("hellBlood").innerHTML = "You have "+Game.Blood+" Blood.";
+	l("bossDamage").innerHTML = Game.bossNames[Game.bossID]+" deals "+Game.bossDamage[Game.bossID]+" damage per second to you.";
+	l("bossHealth").innerHTML = Game.bossNames[Game.bossID]+" has "+Game.currBossHealth+" health remaining.";
 }
 /*==============================================================
   GAME LOOP
@@ -46,13 +61,33 @@ Game.Draw = function()
 Game.Loop = function () {
 	Game.date = new Date();
 	Game.timeRaw = Game.date.getTime();
-	
-	if (Game.timeRaw - Game.timeLast > 1000) {
-		Game.Blood += Game.BloodGainPerSecond;
-		Game.timeLast = Game.date.getTime();
+	if(!Game.isFighting) {
+		if (Game.timeRaw - Game.timeLast > 1000) {
+			Game.Blood += Game.BloodGainPerSecond;
+			Game.timeLast = Game.date.getTime();
+			Game.Draw();
+		}
+	} else if (Game.isFighting) {
+		if (Game.Blood <= 0) {
+			l("altar").style.display = "block";
+			l("hell").style.display = "none";
+			Game.isFighting = false;
+		} 
+		if (Game.currBossHealth <= 0) {
+			l("altar").style.display = "block";
+			l("hell").style.display = "none";
+			Game.bossID += 1;
+			l("fightDemon").innerHTML = "Fight "+Game.bossNames[Game.bossID]
+			Game.isFighting = false;
+		}
+		if (Game.timeRaw - Game.timeLast > 1000) {
+			Game.Blood -= Game.bossDamage[Game.bossID];
+			Game.currBossHealth -= Game.DamagePerSecond;
+			Game.timeLast = Game.date.getTime();
+			Game.Draw();
+		}
 	}
 	
-	Game.Draw();
 	setTimeout(Game.Loop, 33);
 }
 /*==============================================================
@@ -62,9 +97,31 @@ Game.Bleed = function() {
 	if (Game.Blood >= Game.BleedCost) {
 		Game.Blood -= Game.BleedCost;
 		Game.BloodGainPerSecond *= 2;
-		Game.BleedCost *= 3;
+		Game.BleedCost *= 10;
 		Game.Draw();
 	}
+}
+Game.DamageUp = function() {
+	if (Game.Blood >= Game.DamageUpCost) {
+		Game.Blood -= Game.DamageUpCost;
+		Game.DamagePerSecond *= 2;
+		Game.DamageUpCost *= 10;
+		Game.Draw();
+	}
+}
+
+Game.FightNextBoss = function() {
+	l("altar").style.display = "none";
+	l("hell").style.display = "block";
+	l("currentlyFighting").innerHTML = "You are fighting "+Game.bossNames[Game.bossID]+".";
+	Game.isFighting = true;
+	Game.currBossHealth = Game.bossHealth[Game.bossID];
+}
+Game.LeaveHell = function() {
+	l("altar").style.display = "Block";
+	l("hell").style.display = "none";
+	Game.isFighting = false;
+	Game.currBossHealth = Game.bossHealth[Game.bossID];
 }
 
 /*==============================================================
